5fe12fd65dc71aa2185bc229a25f154ca7c01093
Luis Alvarez Sanchez <asluisinfo@gmail.com>
2022-12-27 22:40:24 -0800

    Created the following functions to create a C-based shell: read_line(): * Reads line from user. Dynamically resizes string if needed.
    split_line():
    * Splits line from above and tokenizes it using ' ' as delimiter. In essence, it grabs the individual cmds (only supporting simple commands that don't use complex characters such as " or |.

---
+++ main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void shell_loop();
+char* read_line();
+char ** split_line(char* line);
+
+int main(int argc, char **argv) {
+    shell_loop();
+
+    return 0;
+}
+
+/*
+ * Builds input char by char and dynamically reallocates memory if buffer is full.
+ */
+char* read_line(){
+    int l = 32; // length of string
+    char* input = (char*) malloc(sizeof(char) * l); // str pointer
+    int i = 0; // index on string
+    int c; // character obtained from user
+
+    if(!input){ // couldn't get memory.
+        printf("Failed to allocate memory.\n");
+        exit(-1);
+    }
+
+    // Loop executes until eof or \n char is obtained.
+    while(1){
+        c = getchar(); // get the char
+        if(c == EOF || c == '\n'){
+            c = '\0';
+            *(input + i) = c;
+            return input;
+        }else{
+            *(input + i) = c;
+        }
+
+        i++;
+        if(i >= l){
+            l += 32; // dynamically increase buffer by 32 bytes.
+            input = (char*) realloc(input, l);
+            if(!input){
+                printf("There was an error reallocating memory.\n");
+                exit(-1);
+            }
+        }
+    }
+}
+
+/*
+ * Splits line using ' ' as delimiter. Can dynamically grow size of array being returned.
+ */
+char** split_line(char* line){
+    char* token;
+    int size = 16;
+    char** lines = (char**) malloc(sizeof(char*) * size);
+    int pos = 0;
+    char* delim = " ";
+
+    if(!lines){
+        printf("Issue allocating memory.\n");
+        exit(-1);
+    }
+
+    token = strtok(line, delim); // tokenizing line using ' ' as delimiter.
+    while(token != NULL){  // loops while there are still tokens in line.
+        *(lines + pos) = token;
+        pos++;
+
+        if(pos >= size){ // dynamically reallocating memory if size is too small for list of tokens.
+            size += 32;
+            lines = (char**) realloc(lines, sizeof(char*) * size);
+            if(!lines){
+                printf("error reallocating memory.\n");
+                exit(-1);
+            }
+        }
+        token = strtok(NULL, delim);
+    }
+    *(lines + pos) = NULL;
+    return lines;
+}
+
+void shell_loop(){
+
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = read_line();
+        args = split_line(line);
+        //status = execute(args);
+
+        free(line);
+        free(args);
+    } while(status);
+}
